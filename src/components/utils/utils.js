152aa9bd6beeb35cb522cffeece7eea13abdf5b3
KZhukow <kosty.zhukov.2011@gmail.com>
2023-04-30 10:36:04 +0300

    feat: add utils.js with some functions

---
+++ src/components/utils/utils.js
@@ -0,0 +1,50 @@
+import { enLangKey, languageKeyTypes, storageLanguageKey } from './const';
+
+export function getCurrentLanguage() {
+  const value = localStorage.getItem(storageLanguageKey);
+  if (languageKeyTypes.includes(value)) {
+    return value;
+  }
+  localStorage.setItem(storageLanguageKey, enLangKey);
+  return enLangKey;
+}
+
+export function getKeyboardContainer() {
+  const container = document.createElement('div');
+  container.className = 'keyboard';
+  container.id = 'keyboard';
+  return container;
+}
+
+export function getKeyboardKey(
+  keyCode,
+  width = undefined,
+  symbol = undefined,
+  isIndicator = false,
+) {
+  const key = document.createElement('div');
+  key.className = 'key';
+  key.id = keyCode;
+  if (width !== undefined) {
+    key.style.width = `${width}px`;
+  }
+  if (symbol !== undefined) {
+    key.innerHTML = symbol;
+  }
+  if (isIndicator) {
+    const indicator = document.createElement('div');
+    indicator.className = 'keyIndicator';
+    indicator.id = `${keyCode}Indicator`;
+    key.appendChild(indicator);
+  }
+  return key;
+}
+
+export function getInputField() {
+  const input = document.createElement('textarea');
+  input.setAttribute('autofocus', '');
+  input.spellcheck = false;
+  input.id = 'text';
+  input.className = 'input-text';
+  return input;
+}
