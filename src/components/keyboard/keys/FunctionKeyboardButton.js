667ee92824c94d3a28b3178eb8a98435f382cf06
KZhukow <kosty.zhukov.2011@gmail.com>
2023-04-30 14:25:05 +0300

    fix: delete extra actions from function button component

--- src/components/keyboard/keys/FunctionKeyboardButton.js
+++ src/components/keyboard/keys/FunctionKeyboardButton.js
@@ -2,36 +2,26 @@ import { pressedKeyClassName } from '../../utils/const';
 import { getKeyboardKey } from '../../utils/utils';
 
 export default class FunctionKeyboardButton {
-  constructor(root, keyCode, keyName, keyDownActions, keyUpActions, width = undefined) {
+  constructor(root, keyCode, keyName, actions, width = undefined) {
     this.root = root;
     this.keyCode = keyCode;
     this.keyName = keyName;
-    this.keyDownActions = keyDownActions;
-    this.keyUpActions = keyUpActions;
+    this.keyActions = actions;
 
     this.key = getKeyboardKey(this.keyCode, width, this.keyName);
 
     this.key.addEventListener('mousedown', () => {
-      this.keyDownActions.forEach((action) => action());
+      this.keyActions.forEach((action) => action());
     });
-
-    if (this.keyUpActions !== null) {
-      this.key.addEventListener('mouseup', () => {
-        this.keyUpActions.forEach((action) => action());
-      });
-    }
 
     window.addEventListener('keydown', (e) => {
       if (e.code === this.keyCode) {
-        this.keyDownActions.forEach((action) => action());
+        this.keyActions.forEach((action) => action());
         this.key.classList.add(pressedKeyClassName);
       }
     });
     window.addEventListener('keyup', (e) => {
       if (e.code === this.keyCode) {
-        if (this.keyUpActions !== null) {
-          this.keyUpActions.forEach((action) => { action(); });
-        }
         this.key.classList.remove(pressedKeyClassName);
       }
     });
