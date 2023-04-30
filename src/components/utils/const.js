6be8e01cd16f45668381668ec306ae2b7575738b
KZhukow <kosty.zhukov.2011@gmail.com>
2023-04-30 10:35:39 +0300

    feat: add const.js file with some consts

---
+++ src/components/utils/const.js
@@ -0,0 +1,13 @@
+export const storageLanguageKey = 'language-virtual-keyboard-2023q1-7OFej';
+
+export const ruLangKey = 'ru';
+export const enLangKey = 'en';
+
+export const languageKeyTypes = [enLangKey, ruLangKey];
+
+export const keyboardDefaultStateKey = 'default';
+export const keyboardShiftStateKey = 'shift';
+export const keyboardCapsStateKey = 'caps';
+export const keyboardShiftCapsStateKey = 'shiftCaps';
+
+export const pressedKeyClassName = 'isKeyPressed';
